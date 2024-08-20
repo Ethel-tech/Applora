7723e1f8d8eb10d4215278b28615460e36417df9
Ethel-tech <ethelsimjaja@gmail.com>
2024-08-20 08:01:40 +0100

    features and mobile design edit

--- src/components/Sections/Navigation/Nav.jsx
+++ src/components/Sections/Navigation/Nav.jsx
@@ -1,18 +1,19 @@
-import { IoClose, IoMenu } from "react-icons/io5";
 import applora from "../../../assets/images/Applora.svg";
 import "./navigation.scss";
 import CustomButton from "../../customs/CustomButtons";
 
 function Nav({ navbarOpen, setNavbarOpen }) {
 	return (
-		<nav className="navigation bg-white flex p-10 px-20 items-center justify-between text-xl font-normal text-['#0C335E']">
-			<div className="navigation-logo">
+		<nav className="navigation bg-white flex p-10 md:max-lg:px-10 px-20 max-sm:px-7 max-sm:p-7  items-center justify-between text-xl font-normal text-['#0C335E']">
+			<div className="navigation-logo flex-grow z-20">
 				<img src={applora} alt="logo" className="logo" />
 			</div>
-			<div id="menu-items" className="w-7/12 flex justify-between">
-				<div className="navigation-list-div content-center w-2/5 ">
-					<ul className="navigation-list list-none m-0 p-0 overflow-hidden flex justify-between ">
-						<li className="navigation-list-link">Home</li>
+			<div id="menu-items" className="w-7/12 flex justify-between md:max-lg">
+				<div className="navigation-list-div content-center w-2/5 md:max-lg:w-auto">
+					<ul className="navigation-list list-none m-0 p-0 overflow-hidden flex md:max-lg:gap-5 justify-between ">
+						<a href="#Features">
+							<li className="navigation-list-link">Home</li>
+						</a>
 						<li className="navigation-list-link">About</li>
 						<li className="navigation-list-link">Service</li>
 					</ul>
@@ -28,13 +29,29 @@ function Nav({ navbarOpen, setNavbarOpen }) {
 			</div>
 
 			<button
-				className="md:hidden"
+				className="md:hidden flex top-0 right-0 z-20 relative w-10 h-10 text-white focus:outline-none"
 				id="navigation-toggle"
 				onClick={() => {
 					setNavbarOpen(!navbarOpen);
 				}}
 			>
-				<IoMenu />
+				<div className="absolute w-5 transform -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2">
+					<span
+						className={`absolute h-0.5 w-5 bg-[#58b5ef] transform transition duration-300 ease-in-out ${
+							navbarOpen ? "rotate-45 delay-200" : "-translate-y-1.5"
+						}`}
+					></span>
+					<span
+						className={`absolute h-0.5 bg-[#58b5ef] transform transition-all duration-200 ease-in-out ${
+							navbarOpen ? "w-0 opacity-50" : "w-5 delay-200 opacity-100"
+						}`}
+					></span>
+					<span
+						className={`absolute h-0.5 w-5 bg-[#58b5ef] transform transition duration-300 ease-in-out ${
+							navbarOpen ? "-rotate-45 delay-200" : "translate-y-1.5"
+						}`}
+					></span>
+				</div>
 			</button>
 		</nav>
 	);

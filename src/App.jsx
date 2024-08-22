367352d2b2461d6823800cd104a62412d1e03baa
Ethel-tech <ethelsimjaja@gmail.com>
2024-08-22 17:58:35 +0100

    login and signup pages added

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,21 @@ import Navigation from "./components/Sections/Navigation/Navigation";
 import Services from "./Pages/Services";
 import Footer from "./components/Sections/Footer/footer";
 import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import SignUp from "./Pages/SignUpPage";
 
 // App.jsx
 function App() {
-  return (
-    <>
-	 <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-      <Routes>
-        <Route path="/services" element={<Services />} />
-      </Routes>
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/services" element={<Services />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/sign-up" element={<SignUp />} />
+			</Routes>
+		</>
+	);
 }
 
 export default App;

166d437e5026a34181a5575ab06a0fc1ca5552f8
Ethel-tech <ethelsimjaja@gmail.com>
2024-08-23 13:05:38 +0100

    edits to home branch

--- src/components/customs/CustomButtons.jsx
+++ src/components/customs/CustomButtons.jsx
@@ -3,22 +3,16 @@ const CustomButton = ({
 	onClick = () => {},
 	bgColor = "",
 	txtColor = "",
-<<<<<<< HEAD
-=======
 	border = "",
 	fontSize = "",
 	width,
 	type,
->>>>>>> 63358855fc5e6e93e4b27ce94fa7baebd23ebcd6
 }) => {
 	return (
 		<button
 			type={type}
 			className=" p-2 hover:opacity-90 hover:font-bold rounded-md"
 			onClick={onClick}
-<<<<<<< HEAD
-			style={{ backgroundColor: bgColor, color: txtColor }}
-=======
 			style={{
 				backgroundColor: bgColor,
 				color: txtColor,
@@ -26,7 +20,6 @@ const CustomButton = ({
 				fontSize: fontSize,
 				width: width,
 			}}
->>>>>>> 63358855fc5e6e93e4b27ce94fa7baebd23ebcd6
 		>
 			{btnText}
 		</button>

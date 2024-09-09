6e52758ec7b99dc6b907932f37bd6bc63c8ee536
unknown <elevation2610@gmail.com>
2024-09-10 00:21:24 +0100

    dashboard

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,44 +1,43 @@
 import React from "react";
-
+import { Icon } from "@iconify/react";
 import "../index.css";
 
 function Header() {
   return (
-    <div>
-      <div class="fixed w-full flex items-center justify-between h-14 text-white z-10">
-        <div class="flex items-center justify-start md:justify-center pl-3 w-14 md:w-64 h-14 bg-[#85C1E9] border-none">
-          {/* <span class="hidden md:block">APPLORA</span> */}
-        </div>
-        <div class="flex justify-between items-center h-14 bg-[#85C1E9]  header-right">
-          <div class="bg-[#fff] rounded-lg flex items-center  max-w-xl mr-4 p-0 shadow-sm border border-[#fff]"></div>
-          <ul class="flex items-center">
-            <li>
-              <div class="block w-px h-6 mx-3 bg-gray-400 dark:bg-gray-700"></div>
-            </li>
-            <div class="bg-[#fff] rounded-lg flex items-center  max-w-xl mr-4 p-0 shadow-sm border border-[#fff]">
-              <button class="outline-none focus:outline-none">
-                <svg
-                  class="w-5 text-gray-600 h-5 cursor-pointer"
-                  fill="none"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-                </svg>
-              </button>
-              <input
-                type="search"
-                name=""
-                id=""
-                placeholder="Search Here"
-                class="w-full pl-3 text-sm text-black outline:none bg-transparent rounded-lg border border-[#fff] focus:border-[#fff]"
-              />
-            </div>
-          </ul>
-        </div>
+    <div className="flex fixed w-full items-center justify-between h-14 bg-gray-100">
+      <h2 className="text-black font-bold text-lg justify-center item-center mx-72 ">
+        Welcome,
+        <span className="text-[#F66030]"> Dorothy Ubuara</span>
+      </h2>
+      <div class="">
+        <ul class="flex items-center">
+          <li></li>
+          <div class="bg-[#fff] rounded-lg flex items-center  max-w-xl mr-4 p-0 shadow-sm border border-[#fff]">
+            <button class="outline-none focus:outline-none">
+              <svg
+                class="w-5 text-gray-600 h-5 cursor-pointer"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+              </svg>
+            </button>
+            <input
+              type="search"
+              name=""
+              id=""
+              placeholder="Search Here"
+              class="w-full pl-3 text-sm text-black outline:none bg-transparent rounded-lg border border-[#fff] focus:border-[#fff]"
+            />
+          </div>
+          <span class="inline-flex justify-center items-center ml-2 mx-4">
+            <Icon icon="iwwa:alarm" style={{ fontSize: "23px" }} />
+          </span>
+        </ul>
       </div>
     </div>
   );
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+import { Icon } from "@iconify/react";
+import Woman from "../assets/Woman.png";
 
 import { Button, Dropdown } from "flowbite-react";
 
 function Sidebar() {
   return (
     <div>
-      <div class="fixed flex flex-col top-14 left-0 w-14  md:w-52 bg-[#85C1E9] h-full transition-all duration-300 border-none">
+      <div class="fixed flex flex-col top-22 left-0 w-14  md:w-52 bg-[#85C1E9] z-10 h-full transition-all duration-300 border-none">
         <div class="overflow-y-auto overflow-x-hidden flex flex-col justify-between flex-grow">
-          <ul class="flex flex-col py-4 space-y-1">
+          <ul class="flex flex-col py-4 space-y-0">
             <li class="px-5 hidden md:block">
               <div class="flex flex-row items-center h-8 mt-2">
                 <div class="text-2xl font-bold tracking-wide text-[#007BFF]">
@@ -20,29 +22,12 @@ function Sidebar() {
             <li>
               <a
                 href="#"
-                class="relative flex flex-row items-center h-11 mt-6 mb-8 focus:outline-none text-white-600 mx-2"
+                class="relative flex flex-row items-center h-11 mt-6 mb-2 focus:outline-none text-white-600 mx-2 bg-[#F66030] rounded-md"
               >
-                <span class="inline-flex justify-center items-center ml-4 text-[#09223E]">
-                  <svg
-                    class="w-5 h-5"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-                    ></path>
-                  </svg>
+                <span class="inline-flex justify-center items-center ml-5 text-[#09223E]">
+                  <Icon icon="bxs:dashboard" style={{ fontSize: "23px" }} />
                 </span>
-                <Button.Group outline>
-                  <Button className="px-4 rounded text-[#09223E] justify-center -mx-0 bg-[#F66030] hidden md:block">
-                    Dashboard
-                  </Button>
-                </Button.Group>
+                <h2 className="font-medium mx-3 mt-1 text-white">Dashboard</h2>
               </a>
             </li>
             <hr class="border-1 border-[#000000] w-50 mt-8 gap-8" />
@@ -52,25 +37,15 @@ function Sidebar() {
             <li>
               <a
                 href="#"
-                class="relative flex flex-row items-center text-[#09223E] hover:text-[#F66030] mb-8 h-11 focus:outline-none  border-l-4 border-transparent pr-6"
+                class="relative flex flex-row items-center text-[#09223E] hover:text-[#F66030] mb-8 h-10 focus:outline-none  border-l-4 border-transparent pr-6"
               >
-                <span class="inline-flex justify-center items-center ml-4">
-                  <svg
-                    class="w-5 h-5"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"
-                    ></path>
-                  </svg>
+                <span class="inline-flex justify-center items-center ml-5">
+                  <Icon
+                    icon="material-symbols-light:task-outline"
+                    style={{ fontSize: "23px" }}
+                  />
                 </span>
-                <span class="ml-2 text-sm tracking-wide truncate">Tasks</span>
+                <span class="ml-3 text-sm tracking-wide truncate">Tasks</span>
                 <span class="hidden md:block px-2 py-0.5 ml-auto text-md font-medium tracking-wide text-[#09223E] ">
                   8
                 </span>
@@ -79,25 +54,15 @@ function Sidebar() {
             <li>
               <a
                 href="#"
-                class="relative flex flex-row items-center text-[#09223E] hover:text-[#F66030] h-4 mb-2 focus:outline-none border-l-4 border-transparent pr-6"
+                class="relative flex flex-row items-center text-[#09223E] hover:text-[#F66030] h-1 mb-4 focus:outline-none border-l-4 border-transparent pr-6"
               >
-                <span class="inline-flex justify-center items-center ml-4">
-                  <svg
-                    class="w-5 h-5"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"
-                    ></path>
-                  </svg>
+                <span class="inline-flex justify-center items-center ml-5">
+                  <Icon
+                    icon="dashicons:money-alt"
+                    style={{ fontSize: "23px" }}
+                  />
                 </span>
-                <span class="ml-2 text-sm tracking-wide truncate">Billing</span>
+                <span class="ml-3 text-sm tracking-wide truncate">Billing</span>
               </a>
             </li>
             <hr class="border-1 border-[#000000] w-50 mt-8 gap-8" />
@@ -109,23 +74,13 @@ function Sidebar() {
                 href="#"
                 class="relative flex flex-row items-center text-[#09223E] hover:text-[#F66030] h-4 mb-2 mt-4 focus:outline-none border-l-4 border-transparent pr-6"
               >
-                <span class="inline-flex justify-center items-center ml-4">
-                  <svg
-                    class="w-5 h-5"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                    ></path>
-                  </svg>
+                <span class="inline-flex justify-center items-center ml-5">
+                  <Icon
+                    icon="streamline:notification-alarm-2"
+                    style={{ fontSize: "23px" }}
+                  />
                 </span>
-                <span class="ml-2 text-sm tracking-wide truncate">
+                <span class="ml-3 text-sm tracking-wide truncate">
                   Reminder
                 </span>
               </a>
@@ -136,23 +91,13 @@ function Sidebar() {
                 href="#"
                 class="relative flex flex-row items-center text-[#09223E] hover:text-[#F66030] h-4 mb-2 mt-4 focus:outline-none border-l-4 border-transparent pr-6"
               >
-                <span class="inline-flex justify-center items-center ml-4">
-                  <svg
-                    class="w-5 h-5"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                    ></path>
-                  </svg>
+                <span class="inline-flex justify-center items-center ml-5">
+                  <Icon
+                    icon="ph:note-pencil-thin"
+                    style={{ fontSize: "23px" }}
+                  />
                 </span>
-                <span class="ml-2 text-sm tracking-wide truncate">Notes</span>
+                <span class="ml-3 text-sm tracking-wide truncate">Notes</span>
               </a>
             </li>
 
@@ -161,23 +106,10 @@ function Sidebar() {
                 href="#"
                 class="relative flex flex-row items-center h-4 mb-2 mt-4 focus:outline-none text-[#09223E] hover:text-[#F66030]  border-l-4 border-transparent pr-6"
               >
-                <span class="inline-flex justify-center items-center ml-4">
-                  <svg
-                    class="w-5 h-5"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                    ></path>
-                  </svg>
+                <span class="inline-flex justify-center items-center ml-5">
+                  <Icon icon="ri:team-line" style={{ fontSize: "23px" }} />
                 </span>
-                <div className="mx-2">
+                <div className="mx-3">
                   <Dropdown label="Team" inline>
                     <Dropdown.Item>Earnings</Dropdown.Item>
                     <Dropdown.Item>Earnings</Dropdown.Item>
@@ -191,25 +123,12 @@ function Sidebar() {
             <li>
               <a
                 href="#"
-                class="relative flex flex-row items-center h-4 mb-2 mt-4 focus:outline-none text-[#09223E] hover:text-[#F66030] border-l-4 border-transparent pr-6"
+                class="relative flex flex-row items-center h-4 mb-4 mt-4 focus:outline-none text-[#09223E] hover:text-[#F66030] border-l-4 border-transparent pr-6"
               >
-                <span class="inline-flex justify-center items-center ml-4">
-                  <svg
-                    class="w-5 h-5"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                    ></path>
-                  </svg>
+                <span class="inline-flex justify-center items-center ml-5">
+                  <Icon icon="carbon:customer" style={{ fontSize: "23px" }} />
                 </span>
-                <div className="mx-2">
+                <div className="mx-3">
                   <Dropdown label="Clients" inline>
                     <Dropdown.Item>Earnings</Dropdown.Item>
                     <Dropdown.Item>Earnings</Dropdown.Item>
@@ -219,16 +138,16 @@ function Sidebar() {
                 </div>
               </a>
             </li>
-            <hr class="border-1 border-[#000000] w-50 mt-8 gap-8" />
+            <hr class="border-1 border-[#000000] w-50 mt-2 gap-8" />
             {/* end of reminder, note, team , client */}
 
             {/* profile, setting , support */}
             <li>
               <a
                 href="#"
-                class="relative flex flex-row items-center h-11 mt-4 focus:outline-none text-white-600 text-[#09223E] hover:text-[#F66030] border-l-4 border-transparent pr-6"
+                class="relative flex flex-row items-center h-11 mt-2 focus:outline-none text-white-600 text-[#09223E] hover:text-[#F66030] border-l-4 border-transparent pr-6"
               >
-                <span class="inline-flex justify-center items-center ml-4">
+                <span class="inline-flex justify-center items-center ml-5">
                   <svg
                     class="w-5 h-5"
                     fill="none"
@@ -244,7 +163,7 @@ function Sidebar() {
                     ></path>
                   </svg>
                 </span>
-                <span class="ml-2 text-sm tracking-wide truncate">
+                <span class="ml-3 text-sm tracking-wide truncate">
                   My Profile
                 </span>
               </a>
@@ -255,7 +174,7 @@ function Sidebar() {
                 href="#"
                 class="relative flex flex-row items-center h-11 focus:outline-none text-[#09223E] hover:text-[#F66030] border-l-4 border-transparent pr-6"
               >
-                <span class="inline-flex justify-center items-center ml-4">
+                <span class="inline-flex justify-center items-center ml-5">
                   <svg
                     class="w-5 h-5"
                     fill="none"
@@ -277,7 +196,7 @@ function Sidebar() {
                     ></path>
                   </svg>
                 </span>
-                <span class="ml-2 text-sm tracking-wide truncate">
+                <span class="ml-3 text-sm tracking-wide truncate">
                   Settings
                 </span>
               </a>
@@ -286,36 +205,33 @@ function Sidebar() {
             <li>
               <a
                 href="#"
-                class="relative flex flex-row items-center h-11 focus:outline-none text-[#09223E] hover:text-[#F66030] border-l-4 border-transparent pr-6"
+                class="relative flex flex-row items-center h-11 mb-2 focus:outline-none text-[#09223E] hover:text-[#F66030] border-l-4 border-transparent pr-6"
               >
-                <span class="inline-flex justify-center items-center ml-4">
-                  <svg
-                    class="w-5 h-5"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
-                    ></path>
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
-                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                    ></path>
-                  </svg>
+                <span class="inline-flex justify-center items-center ml-5">
+                  <Icon
+                    icon="streamline:call-center-support-service"
+                    style={{ fontSize: "23px" }}
+                  />
                 </span>
-                <span class="ml-2 text-sm tracking-wide truncate">Support</span>
+                <span class="ml-3 text-sm tracking-wide truncate">Support</span>
               </a>
             </li>
             {/* end of profile , setting, support */}
+
+            <div className="bg-[#F66030]  items-center w-full h-14 mt-12 pt-2 pb-8 focus:outline-none text-white-600 mx-0 rounded-md">
+              <span className="flex flex-1">
+                <span class="inline-flex justify-center items-center ml-4 text-[#09223E] border-2 border-white rounded-full">
+                  <img src={Woman} alt="woman" className="w-6" />
+                </span>{" "}
+                <h2 className="mx-2 font-medium mt-1 text-white">
+                  Dorothy Ubuara
+                </h2>
+              </span>
+              <h3 className="mx-3 inset-1/2 font-normal text-center text-white text-sm">
+                dorothyubuara@gmail.com
+              </h3>
+            </div>
           </ul>
-         
         </div>
       </div>
     </div>
